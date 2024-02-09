0a080b0562b8241a05e32a5e56de7d98af2f2f4c
Dezső Tókos <dezsotokos@minic.ro>
2024-02-09 12:39:22 +0200

    useeffect

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,29 @@ import MadiaPlayer from "./MadiaPlayer";
 import Calculator from "./Calculator";
 import video from '../assets/videos/front_vid.mp4'
 import GoalManagger from "./GoalManagger";
+import { useEffect, useState } from "react";
 
 function HomePage(props) {
     const day = new Date().getDay();
+    const [toggle, setToggle] = useState(false);
+    const [toggle2, setToggle2] = useState(false);
     const alignCenter = {
         display: 'flex',
         alignItems: 'center',
         flexDirection: 'column',
     };
 
+    const clickHandler = () => {
+        setToggle(!toggle);
+    }
+
+    const clickHandler2 = () => {
+        setToggle2(!toggle2);
+    }
+    useEffect(() => {
+        console.log('useeffect')
+        document.title = toggle ? "Welcome to MyHouse" : "Using useEffect";
+    }, [toggle])
     return (
         <div>
             <h1>Welcome to Home site</h1>
@@ -21,6 +35,14 @@ function HomePage(props) {
                 ? <Workdays />
                 : <Weekend />
             }
+
+            <button onClick={clickHandler}>
+                Toggle message
+            </button>
+            <button onClick={clickHandler2}>
+                Toggle2 message
+            </button>
+            {toggle && <h2>Toggle is set on</h2>}
             <MadiaPlayer 
                 style={alignCenter}
                 url={video}
