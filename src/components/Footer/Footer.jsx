5d0a5bc14ebdecb955e70b7ffac05a4e596efc87
tdim1582 <dezsotokos@gmail.com>
2024-02-22 22:22:07 +0200

    footer trasn and some style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,37 +1,38 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
+    const { t } = useTranslation();
     return (
         <div className="footer">
             <div class="footer-container">
-                <div class="footer-element">
-                    <div class="footer-element-logo">
-                        <h4 class="footer-logo-title">Lily Guesthouse</h4>
+                <div class="footer-element footer-logo">
+                    {/* <div class="footer-logo"> */}
+                        <h4 class="footer-logo-title">{t("footer.name")}</h4>
                         <a href="/">
-                            <img width="150" height="150" src="" class="image" alt="" title="Gyöngyvirág Kulcsosház" />
+                            <img width="150" height="150" src="" class="image" alt="" title={t("footer.name")} />
                         </a>
-                    </div>        
+                    {/* </div>         */}
                 </div>
-                <div class="footer-element">
-                    <div class="footer-element-address">
+                <div class="footer-element footer-address">
+                    {/* <div class="footer-address"> */}
                         <p>
-                            Szeltersz, 1304/B, Lövéte, Románia
+                            {t("footer.address")}
                         </p>
-                    </div>
+                    {/* </div> */}
                 </div>
-                <div class="footer-element">
-                    <div class="footer-element-nav_menu">
-                        <ul class="footer-nav-menu">
-                            <li class="footer-nav-menu-item"><a href="/about-me/">About the guesthouse</a></li>
-                            <li class="footer-nav-menu-item"><a href="#">Contact</a></li>
-                            <li class="footer-nav-menu-item"><a href="#">Services</a></li>
-                            <li class="footer-nav-menu-item"><a href="#">Sights</a></li>
-                        </ul>
-                    </div>
+                <div class="footer-element footer-nav_menu">
+                    {/* <div class="footer-nav_menu"> */}
+                        <a href="/about/">About the guesthouse</a>
+                        <a href="#">Contact</a>
+                        <a href="#">Services</a>
+                        <a href="#">Sights</a>
+                    {/* </div> */}
                 </div>
-                <div class="footer-element">
-                    <div class="footer-element-contact">
-                        <h4 class="footer-contact-title">Book your stay:</h4>
+                <div class="footer-element footer-contact">
+                    {/* <div class="footer-contact"> */}
+                        <h4 class="footer-contact-title">{t("footer.bookable_notice")}</h4>
                         <div class="footer-contact-container">
                             <p>
                                 Tel:<br/>
@@ -43,8 +44,15 @@ const Footer = () => {
                                 dezsotokos@gmail.com
                             </p>
                         </div>
-                    </div>       
+                    {/* </div> */}
                  </div>
+            </div>
+            <div className="footer-below">
+                <div className="footer-copyright">
+                    <p>
+                        @{new Date().getFullYear()} Gyöngyvirág Kulcsosház. All right reserved.
+                    </p>
+                </div>
             </div>
         </div>
     );
